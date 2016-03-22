37400c085524248f479d55d72b0a88aadb38bee7
LukeRibbons <luke.ribbons@my.jcu.edu.au>
2016-03-22 17:36:24 +1000

    Workshops

--- Workshop 3/ASCII.py
+++ Workshop 3/ASCII.py
@@ -3,7 +3,7 @@ UPPER = 100
 
 as_integer = int(input("Enter a number ({}-{}):".format(LOWER, UPPER)).strip())
 
-while as_integer >= 10 and as_integer <= 100:
+while 10 <= as_integer <= 100:
     print("{:<3} {}".format(as_integer, chr(as_integer)))
     as_integer = int(input("Enter a number ({}-{}):".format(LOWER, UPPER)))
 
@@ -11,3 +11,14 @@ print("You have input an invalid number, program end.")
 
 for i in range(10, 120, 11):
     print("{:>3} {}".format(i, chr(i)))
+
+
+def get_number(lower, upper):
+    entered_number = input("Enter a number ({}-{}):".format(lower, upper))
+    while lower >= int(entered_number) >= upper or not entered_number.isdecimal():
+        print("Please enter a valid number")
+        entered_number = input("Enter a number ({}-{}):".format(lower, upper))
+    return entered_number
+number_entered = int(get_number(LOWER, UPPER))
+
+print("{:<3} {}".format(number_entered, chr(number_entered)))
