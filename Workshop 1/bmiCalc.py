20b0682f87361f7b8a535217e1decd3a79943e5f
LukeRibbons <luke.ribbons@my.jcu.edu.au>
2016-03-22 16:34:12 +1000

    Workshops

--- Workshop 1/bmiCalc.py
+++ Workshop 1/bmiCalc.py
@@ -1,9 +1,12 @@
 print('Body-mass-index calculator, by Luke')
 
-weight_value = float(input('Please enter your weight in kgs:'))
-height_value = float(input('Please enter your height in m:'))
 
-bmi_value = weight_value / height_value ** 2
+def calculate_bmi():
+    weight_value = float(input('Please enter your weight in kgs:'))
+    height_value = float(input('Please enter your height in m:'))
+    bmi_calculated = weight_value / height_value ** 2
+    return bmi_calculated
 
+bmi_value = calculate_bmi()
 print('Therefore, your BMI value is:', bmi_value)
 print('thank you!')
