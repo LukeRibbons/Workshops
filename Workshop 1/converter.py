1e680c55b66234bff1338b705a1fe1ee87d08087
LukeRibbons <luke.ribbons@my.jcu.edu.au>
2016-03-22 16:31:42 +1000

    Workshops

--- Workshop 1/converter.py
+++ Workshop 1/converter.py
@@ -1,9 +1,13 @@
 print('Temperature Conversion program')
 
-celsius_value = float(input('Enter temperature in celsius:'))
-fahrenheit_value = celsius_value * 9 / 5 + 32
-kelvin_value = celsius_value + 273.15
 
-print('celsius value:', celsius_value)
-print('fahrenheit value:', fahrenheit_value)
-print('kelvin value:', kelvin_value)
+def convert_celsius_to_fahrenheit():
+    celsius_value = float(input('Enter temperature in celsius:'))
+    fahrenheit_value = celsius_value * 9 / 5 + 32
+    kelvin_value = celsius_value + 273.15
+    return celsius_value, fahrenheit_value, kelvin_value
+
+temperature_tuple = convert_celsius_to_fahrenheit()
+print('celsius value:', temperature_tuple[0])
+print('fahrenheit value:', temperature_tuple[1])
+print('kelvin value:', temperature_tuple[2])
