aab7792e6f8bf20e9eaa3e64ba69bf91c9c2e7db
LukeRibbons <luke.ribbons@my.jcu.edu.au>
2016-03-23 12:31:54 +1000

    Workshops

--- Workshop 1/calcAreaAndVolume.py
+++ Workshop 1/calcAreaAndVolume.py
@@ -1,14 +1,14 @@
 print('Area calculator')
 
 depth = float(input('enter depth:'))
+width = float(input('enter width:'))
+height = float(input('enter height:'))
 
 
-def calculate_area():
-    width = float(input('enter width:'))
-    height = float(input('enter height:'))
+def calculate_area(width, height):
     rectangle_area = width * height
     return rectangle_area
-area = calculate_area()
+area = calculate_area(width, height)
 volume = area * depth
 
 print('Area:', area, 'Volume:', volume)
