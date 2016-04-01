7d3c7f22b8ca6c2585d088a6e6c15fef4eea7963
LukeRibbons <luke.ribbons@my.jcu.edu.au>
2016-04-01 14:41:31 +1000

    Workshops

--- Workshop 5/lottery.py
+++ Workshop 5/lottery.py
@@ -1,10 +1,15 @@
 import random
+PICK_LENGTH = 6
+MINIMUM = 1
+MAXIMUM = 45
 picks = int(input("How many quick picks? "))
 for i in range(picks):
-    pick_list = [0] * 6
-    for a in range(6):
-        pick_list[a] = random.randint(1, 45)
+    pick_list = [0] * PICK_LENGTH
+    for a in range(PICK_LENGTH):
+        pick_list[a] = random.randint(MINIMUM, MAXIMUM)
         while pick_list.count(pick_list[a]) >= 2:
-            pick_list[a] = random.randint(1, 45)
+            pick_list[a] = random.randint(MINIMUM, MAXIMUM)
     pick_list = sorted(pick_list)
-    print("{:3} {:3} {:3} {:3} {:3} {:3}".format(pick_list[0], pick_list[1], pick_list[2], pick_list[3], pick_list[4], pick_list[5]))
+    for a in range(PICK_LENGTH):
+        print("{:3}".format(pick_list[a]), end=" ")
+    print()
