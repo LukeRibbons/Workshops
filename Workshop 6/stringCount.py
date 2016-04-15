a71a40818df7b8fc874e058b8f73bcd975226504
LukeRibbons <luke.ribbons@my.jcu.edu.au>
2016-04-15 14:09:04 +1000

    Workshops

--- Workshop 6/stringCount.py
+++ Workshop 6/stringCount.py
@@ -1,12 +1,8 @@
 user_string = input("Text: ")
 words = user_string.split(" ")
-words_keys_to_sort = []
-sorted(words)
 word_dictionary_count = {}
 for i in range(len(words)):
     if not words[i] in word_dictionary_count:
         word_dictionary_count[words[i]] = words.count(words[i])
-        words_keys_to_sort.append(words[i])
-    words_keys_to_sort.sort()
-for keys in words_keys_to_sort:
-    print(keys, " : ", word_dictionary_count[keys])  # TODO find better way to do this
+for key in sorted(word_dictionary_count):
+    print(key, " : ", word_dictionary_count[key])
