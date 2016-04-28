d8cfa45492c278a9e590969900fc77a03b324648
LukeRibbons <luke.ribbons@my.jcu.edu.au>
2016-04-29 08:11:52 +1000

    Workshops

--- Workshop 6/stringCount.py
+++ Workshop 6/stringCount.py
@@ -1,8 +1,7 @@
 user_string = input("Text: ")
 words = user_string.split(" ")
 word_dictionary_count = {}
-for i in range(len(words)):
-    if not words[i] in word_dictionary_count:
-        word_dictionary_count[words[i]] = words.count(words[i])
+for word in set(words):
+    word_dictionary_count[word] = words.count(word)
 for key in sorted(word_dictionary_count):
     print(key, " : ", word_dictionary_count[key])
