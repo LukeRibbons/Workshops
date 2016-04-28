fc2d70a12bb9d9818fa066bdabacc2ac9bd608ae
LukeRibbons <luke.ribbons@my.jcu.edu.au>
2016-04-29 08:28:32 +1000

    Workshops

---
+++ Workshop 8/usedcars.py
@@ -0,0 +1,16 @@
+from car import Car
+
+
+def main():
+    bus = Car("Bus", 180)
+    bus.drive(30)
+    print("fuel =", bus.fuel)
+    print("odo =", bus.odometer)
+    print(bus)
+    limo = Car("Limo", 100)
+    limo.add_fuel(20)
+    print("fuel =", limo.fuel)
+    limo.drive(115)
+    print("odo =", limo.odometer)
+
+main()
