ad958e3ddad3c6e5e31e3610dd55dbbd96abb246
LukeRibbons <luke.ribbons@my.jcu.edu.au>
2016-04-29 08:28:14 +1000

    Workshops

--- Workshop 8/car.py
+++ Workshop 8/car.py
@@ -2,9 +2,10 @@
 
 
 class Car:
-    def __init__(self, fuel=0):
+    def __init__(self, name="Car", fuel=0):
         """ initialise a Car instance
         fuel: float, one unit of fuel drives one kilometre """
+        self.name = name
         self.fuel = fuel
         self.odometer = 0
 
@@ -23,3 +24,6 @@ class Car:
             self.fuel -= distance
         self.odometer += distance
         return distance
+
+    def __str__(self):
+        return "{}, fuel={}, odometer={}".format(self.name, self.fuel, self.odometer)
